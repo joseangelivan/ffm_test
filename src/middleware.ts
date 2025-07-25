fbfae77a0e2427b97c18a9584fce73c28bbddf51
Jose Angel Ivan Rubianes Silva <angelivan34@gmail.com>
2025-07-25 05:54:55 +0000

    cuando estoy logado y vuelvo hacia atras aun aprece la pagina de login.

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,33 +9,31 @@ const JWT_SECRET = new TextEncoder().encode(
     'your-super-secret-key-that-is-at-least-32-bytes-long'
 );
 
-export async function middleware(request: NextRequest) {
-  const sessionToken = request.cookies.get('session')?.value;
-  const {pathname} = request.nextUrl;
-
-  const isLoginPage = pathname === '/admin/login';
-  
-  let isSessionValid = false;
-  if(sessionToken) {
+const getSession = async (token: string | undefined) => {
+    if (!token) return null;
     try {
-      await jwtVerify(sessionToken, JWT_SECRET);
-      isSessionValid = true;
-    } catch(err) {
-      isSessionValid = false;
+        const { payload } = await jwtVerify(token, JWT_SECRET);
+        return payload;
+    } catch (error) {
+        return null;
     }
+}
+
+export async function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+  const sessionToken = request.cookies.get('session')?.value;
+  
+  const session = await getSession(sessionToken);
+
+  const isTryingToAccessProtectedRoute = pathname.startsWith('/admin') && pathname !== '/admin/login';
+  const isTryingToAccessLoginPage = pathname === '/admin/login';
+
+  if (!session && isTryingToAccessProtectedRoute) {
+    return NextResponse.redirect(new URL('/admin/login', request.url));
   }
 
-
-  if (isSessionValid) {
-    // If the user is logged in and tries to access the login page, redirect to dashboard
-    if (isLoginPage) {
-      return NextResponse.redirect(new URL('/admin/dashboard', request.url));
-    }
-  } else {
-    // If the user is not logged in and tries to access any page other than login, redirect to login
-    if (!isLoginPage) {
-      return NextResponse.redirect(new URL('/admin/login', request.url));
-    }
+  if (session && isTryingToAccessLoginPage) {
+     return NextResponse.redirect(new URL('/admin/dashboard', request.url));
   }
   
   return NextResponse.next();
