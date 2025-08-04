73b23c0b57ad530438062d4825e76195e21c024c
Jose Angel Ivan Rubianes Silva <angelivan34@gmail.com>
2025-08-04 20:12:10 +0000

    react-dom-client.development.js:25022 Download the React DevTools for a

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -2,19 +2,8 @@
 import AdminLoginForm from '@/components/admin-login-form';
 import { Suspense } from 'react';
 import Loading from '@/app/loading';
-import { getSession } from '@/lib/session';
-import { redirect } from 'next/navigation';
-import { cookies } from 'next/headers';
 
-export default async function AdminLoginPage() {
-  const cookieStore = cookies();
-  const sessionToken = cookieStore.get('session')?.value;
-  const session = await getSession(sessionToken);
-
-  if (session?.type === 'admin') {
-    redirect('/admin/dashboard');
-  }
-
+export default function AdminLoginPage() {
   return (
     <Suspense fallback={<Loading />}>
       <AdminLoginForm />
