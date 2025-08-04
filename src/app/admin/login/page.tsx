350be29364ef9e0eb97476be0f05d81988bcfe6d
Jose Angel Ivan Rubianes Silva <angelivan34@gmail.com>
2025-08-04 18:52:12 +0000

    al pasar del login principal al login de administrador:
    page.tsx:15  Se

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -1,4 +1,3 @@
-
 import AdminLoginForm from '@/components/admin-login-form';
 import { Suspense } from 'react';
 import Loading from '@/app/loading';
@@ -6,7 +5,7 @@ import { DbInitializer } from '@/components/admin/db-initializer';
 
 // This page now handles the init_db search parameter to show the
 // database initialization UI, or the standard login form.
-export default function AdminLoginPage({
+export default async function AdminLoginPage({
   searchParams,
 }: {
   searchParams?: { [key: string]: string | string[] | undefined };
