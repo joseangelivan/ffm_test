f2d63f1238fcdac256654f9c82f9bd440f40aab2
Jose Angel Ivan Rubianes Silva <angelivan34@gmail.com>
2025-08-11 04:09:35 +0000

    las repsuesta de error de background rojo deberina tener la opción de co

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -1,6 +1,7 @@
+
 "use client"
 
-import { useToast } from "@/hooks/use-toast"
+import { useToast, toast as toastFn } from "@/hooks/use-toast"
 import {
   Toast,
   ToastClose,
@@ -9,9 +10,23 @@ import {
   ToastTitle,
   ToastViewport,
 } from "@/components/ui/toast"
+import { Button } from "./button";
+import { Copy } from "lucide-react";
+import { useLocale } from "@/lib/i18n";
 
 export function Toaster() {
   const { toasts } = useToast()
+  const { t } = useLocale()
+
+  const handleCopy = (title: React.ReactNode, description: React.ReactNode) => {
+    const textToCopy = `${title}\n${description}`;
+    navigator.clipboard.writeText(textToCopy);
+    toastFn({
+      title: t('toast.copied.title'),
+      description: t('toast.copied.description'),
+    });
+  };
+
 
   return (
     <ToastProvider>
@@ -21,11 +36,23 @@ export function Toaster() {
             <div className="grid gap-1">
               {title && <ToastTitle>{title}</ToastTitle>}
               {description && (
-                <ToastDescription variant={props.variant}>{description}</ToastDescription>
+                <ToastDescription>{description}</ToastDescription>
               )}
             </div>
-            {action}
-            <ToastClose />
+            <div className="flex items-center gap-2">
+              {action}
+              {props.variant === "destructive" && (
+                  <Button
+                    variant="outline"
+                    size="icon"
+                    className="h-8 w-8 group-[.destructive]:border-destructive-foreground/20 group-[.destructive]:hover:bg-destructive-foreground/10 group-[.destructive]:text-destructive-foreground"
+                    onClick={() => handleCopy(title, description)}
+                  >
+                    <Copy className="h-4 w-4" />
+                  </Button>
+              )}
+               <ToastClose />
+            </div>
           </Toast>
         )
       })}
